17c33229b04634141059be89f0663b62b49fa243
coderjins <coderjins@gmail.com>
2023-12-25 15:05:38 +0900

    feat: 1차완성본

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:contacts_service/contacts_service.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-          home: MyApp()
-      )
-  );
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -18,8 +16,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var name = ['유진서', '강승찬', '청룡이'];
-  String inputText = ''; // 사용자 입력을 저장할 변수
+  getPermission() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print('허락됨');
+      refreshContacts();
+    } else if (status.isDenied) {
+      print('거절됨');
+      Permission.contacts.request();
+    }
+  }
+
+  List<Contact> contacts = []; // 연락처 목록을 저장할 변수
+
+  // 연락처 목록을 새로고침하는 함수
+  refreshContacts() async {
+    var newContacts = await ContactsService.getContacts();
+    setState(() {
+      contacts = newContacts;
+    });
+  }
 
   @override
   build(context) {
@@ -30,23 +46,36 @@ class _MyAppState extends State<MyApp> {
             context: context,
             builder: (context) {
               return DialogUI(
-                onSubmitted: (inputText) {
-                  setState(() {
-                    name.add(inputText);
-                  });
+                onSubmitted: (inputName) async {
+                  var newPerson = Contact();
+                  // 이름 분리 (예시: '내린천 별빛')
+                  var names = inputName.split(' ');
+                  if (names.length > 1) {
+                    newPerson.givenName = names[0]; // 주어진 이름
+                    newPerson.familyName = names[1]; // 가족 이름
+                  } else {
+                    newPerson.givenName = inputName; // 이름만 있는 경우
+                  }
+                  await ContactsService.addContact(newPerson); // 연락처에 추가
+                  refreshContacts(); // 연락처 목록 새로고침
                 },
               );
             },
           );
         },
       ),
-      appBar: AppBar(title: Text('연락처앱')),
+      appBar: AppBar(
+        title: Text('연락처앱'),
+        actions: [
+          IconButton(onPressed: getPermission, icon: Icon(Icons.contacts))
+        ],
+      ),
       body: ListView.builder(
-        itemCount: name.length,
+        itemCount: contacts.length,
         itemBuilder: (context, i) {
           return ListTile(
             leading: Image.asset('assets/profile.png'),
-            title: Text(name[i]),
+            title: Text(contacts[i].givenName ?? ''), // 연락처 이름 표시
           );
         },
       ),
@@ -54,6 +83,7 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
 //stless 로 커스텀위젯을 만들 수 있다!
 class BottomAppBar extends StatelessWidget {
   const BottomAppBar({super.key});
